0e9e16355f760eb31245f3237504ac69f29ece7a
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-27 19:19:28 +0330

    export models

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,11 @@
+const models = require('../models');
+
 module.exports = {
     mongodb: {
         url: process.env.MONGODB_DATABASE_URL,
         options: {
             useNewUrlParser: true
-        }
+        },
+        models
     }
 };
---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const Category = require('./category');
+const Article = require('./article');
+const User = require('./user');
+
+module.exports = {
+    Article,
+    Category,
+    User
+};
