2b9ff5028781045a4e91aec52501125dcb5e2c14
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-27 18:40:57 +0330

    create user schema

---
+++ server/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: null
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        default:'user' 
+    },
+    avatar: {
+        type: String
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    activeCode: {
+        type: String,
+        default: '1234'
+    },
+    bookmarks: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true,
+        ref: 'Article'
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
