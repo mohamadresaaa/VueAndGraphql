1e6730f4514558fa092b02070506a25c07fd5997
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-30 16:18:50 +0330

    custom apollo

--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,16 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vueApollo'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { apolloProvider } from './apollo'
 import './registerServiceWorker'
-import ApolloClient from 'apollo-boost'
-import VueApollo from 'vue-apollo'
-
-// use vue apollo
-Vue.use(VueApollo);
-
-// setup apollo client
-const defaultClient = new ApolloClient({
-  uri: 'http://localhost:2000'
-});
-
-const apolloProvider = new VueApollo({ defaultClient });
 
 Vue.config.productionTip = false
 
 new Vue({
-  provide: apolloProvider.provide(),
+  apolloProvider,
   router,
   store,
   render: h => h(App)
