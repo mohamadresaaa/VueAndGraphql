b8af415726d2902585e41937258f4be6ab7de537
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-30 16:19:12 +0330

    loading page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
-    <h1>This is an home page {{ names }}</h1>
+    <h1>This is an home page</h1>
+    <div v-show="$apollo.loading" class="font-weight-medium text-capitalize">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+      loading...
+    </div>
+    <div v-if="!$apollo.loading">
+      my team
+      <ul>
+        <li v-for="(name, index) in names" :key="index">
+          {{name}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
