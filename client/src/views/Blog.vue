0c08629811bbac619f4848aa978f9dee557aed78
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-31 22:14:14 +0330

    add category menu in blog page

--- client/src/views/Blog.vue
+++ client/src/views/Blog.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <h1 class="font-weight-medium text-capitalize mb-3">blog</h1>
-      <v-layout justify-center align-center wrap>
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indeterminate :size="100" :width="10" color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
+
+    <v-menu v-if="!loading" min-width=200 offset-y>
+      <v-btn class="text-capitalize" slot="activator" color="secondary" dark>
+        Categories
+      </v-btn>
+      <v-list>
+        <v-list-tile v-for="(category, index) in categories" :key="index">
+          <v-list-tile-title>{{ index+1 }}.{{ category.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+      <v-layout v-if="!loading" justify-center align-center wrap>
         <v-flex xs12>
             <v-layout row wrap>
               <v-flex class="pa-2" xs12 sm6 md4 v-for="(article, index) in articles" :key="index">
@@ -29,6 +49,7 @@
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
@@ -37,19 +58,19 @@ export default {
         { title: 'Code up the homepage', person:'saeed oraji', due: '10th Jan 2019', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Design video thumbnails', person:'ali mohamadi', due: '20th Dec 2018', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person:'the ninja', due: '20th Oct 2018', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-      ],
-      items: [
-        {
-          text: 'Home',
-          disabled: false,
-          href: '/'
-        },
-        {
-          text: 'Blog',
-          disabled: true,
-          href: '/blog'
-        }
       ]
+    }
+  },
+  created() {
+    this.handleGetCategories();
+  },
+  computed: {
+    ...mapGetters(["loading", "categories"])
+  },
+  methods: {
+    handleGetCategories() {
+      // reach out to Vuex store, fire action that gets posts for carousel
+      this.$store.dispatch("getCategories");
     }
   }
 }
