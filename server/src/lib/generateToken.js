9d5da9226088c7ead952b51bae29ef7ebb11e48d
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-04 20:20:02 +0330

    update generate and verify token

--- server/src/lib/generateToken.js
+++ server/src/lib/generateToken.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = async (user, secretKey) => {
     return await jwt.sign({
-            id: user._id,
-            username: user.username
+            id: user._id
         },
         secretKey
     );
--- server/src/lib/verifyToken.js
+++ server/src/lib/verifyToken.js
@@ -4,8 +4,17 @@ const { AuthenticationError } = require('apollo-server-express');
 module.exports = async token => {
     if (token) {
         try {
-            let user = await jwt.verify(token, 'secretKey');
-            console.log(user);
+            // get user id
+            let { id } = await jwt.verify(token, 'secretKey');
+
+            // find user
+            let user = await config.database.mongodb.models.User.findById(id);
+
+            // if user doesn't exists
+            if(!user) throw new Error('Token is not valid.');
+
+            // return it
+            return user;
         } catch (error) {
             throw new AuthenticationError('Your session has ended! Please sign in again.');
         }
