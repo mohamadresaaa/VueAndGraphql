ab30503b3b153cb55bf23f0f213a020860ba8c9f
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-07 00:52:50 +0330

    signup mutation was added

--- client/src/graphql/mutations/authenticate.js
+++ client/src/graphql/mutations/authenticate.js
@@ -1,5 +1,16 @@
 import { gql } from "apollo-boost";
 
+export const SIGN_UP = gql `
+  mutation ($username: String!, $email: String!, $password: String!){
+    signUp(username: $username, email: $email, password: $password){
+      username
+      email
+      joinDate
+      avatar
+    }
+  }
+`;
+
 export const SIGN_IN = gql `
   mutation ($email: String!, $password: String!){
     signIn(email: $email, password: $password){
