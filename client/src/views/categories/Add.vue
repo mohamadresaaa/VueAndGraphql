7c808899132588947369dc9611af1f2213ff76ab
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-09 16:44:41 +0330

    add category validation

--- client/src/views/categories/Add.vue
+++ client/src/views/categories/Add.vue
@@ -5,14 +5,43 @@
                 <h2>Add a New Category</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3" ref="form">
-                    <v-text-field v-model="title" label="Title" prepend-icon="title"></v-text-field>
-                    <v-text-field v-model="url" label="Url"></v-text-field>
+                <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleAddCategory" class="px-3">
+                    <v-text-field v-model="title" :rules="titleRules" label="Title" prepend-icon="title"></v-text-field>
+                    <v-text-field v-model="url" :rules="urlRules" label="Url"></v-text-field>
                     <v-spacer></v-spacer>
-                    <v-btn flat @click="submit" class="success mt-3 mr-1">Save</v-btn>
-                    <v-btn flat router to="/admin/categories" class="black white--text mt-3 mx-1">Back</v-btn>
+                    <v-btn :loading="loading" :disabled="!isFormValid || loading" color="success" class="mt-3 mr-1" type="submit">
+                        Save
+                        <span slot="loader">Loading...</span>
+                    </v-btn>
+                    <v-btn router to="/admin/categories" class="mt-3 mx-1">Back</v-btn>
                 </v-form>
             </v-card-text>
         </v-card>
     </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    data() {
+        return {
+            title: '',
+            url: '',
+            titleRules: [title => !!title || 'Title is required'],
+            urlRules: [url => !!url || 'Url is required'],
+            isFormValid: true
+        }
+    },
+    computed: {
+        ...mapGetters(['loading'])
+    },
+    methods: {
+        handleAddCategory(){
+            if(this.$refs.form.validate()){
+                // use dispatch
+            }
+        }
+    },
+}
+</script>
