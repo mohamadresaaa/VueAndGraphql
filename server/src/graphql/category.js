6806bb3e1fb1535cd4b7c44c92114d9a845c6161
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-13 12:54:27 +0330

    generate category url

--- server/src/graphql/category.js
+++ server/src/graphql/category.js
@@ -1,8 +1,11 @@
+import { slug } from '../lib/regex';
+
 export const getCategories = async (_, args, { Category }) => (await Category.find());
 
 export const addCategory = async (_, { title, url }, { Category }) => {
     // set url
-
+    url = url.replace(slug, '-');
+    
     // checked title exists
     const titleExists = await Category.findOne({ title });
     if(titleExists) throw new Error('Title is already taken');
