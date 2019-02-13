6cf322bfcf2bc3942089091033d91fb93e797656
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-13 12:31:34 +0330

    regex was added

---
+++ server/src/lib/regex.js
@@ -0,0 +1,5 @@
+export default {
+    separatingEmail : /(@[\w.]+)/, // remove @gmail.com or @exmple.com
+    validationUsername : /^[a-zA-Z0-9\-\.]+$/, // just alphanumeric, (- , .) and numeric characters
+    slug : /([^۰-۹آ-یa-z0-9]|-)+/g 
+};
