3f00f7a1ae30b7dbeb7fa73a6d70b8f6a64ca6bc
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-02-14 16:33:25 +0330

    server: change password process

--- server/src/graphql/mutations.js
+++ server/src/graphql/mutations.js
@@ -2,10 +2,12 @@ import { signUp, signIn } from './authenticate';
 import { addCategory } from './category';
 import { addTodo } from './todo';
 import { addNote } from './note';
+import { changePassword } from './user';
 
 module.exports = {
     signUp,
     signIn,
+    changePassword,
     addCategory,
     addTodo,
     addNote
--- server/src/graphql/user.js
+++ server/src/graphql/user.js
@@ -1,3 +1,24 @@
 export const getCurrentUser = async (_, args, { User, currentUserId }) => {
     return await User.findById(currentUserId);
 };
+
+export const changePassword = async (_, { oldPassword, newPassword, userId }, { User }) => {
+    // find user
+    let user = await User.findById(userId);
+
+    // if not, handle it
+    if(!user) throw new Error('User not found');
+
+    // compare password
+    let isMatch = await user.comparePassword(oldPassword);
+
+    // if the password was not equal
+    if(!isMatch) throw new Error('Incorrect password');
+
+    // set new password
+    user.password = newPassword;
+    await user.save();
+
+    // return message
+    return { message: 'Password was successfully changed.' };
+};
