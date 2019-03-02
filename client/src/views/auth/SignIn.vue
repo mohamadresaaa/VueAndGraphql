254e8599991e931202ce45510bb56dae4814e711
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-03-02 23:29:28 +0330

    edit sign in and sign up form

--- client/src/views/auth/SignIn.vue
+++ client/src/views/auth/SignIn.vue
@@ -2,30 +2,30 @@
     <v-layout align-center justify-center>
         <v-flex xs12 sm8 md6>
             <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Sign in</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form v-model="isFormValid" lazy-validation ref="form">
-                  <v-text-field v-model="email" :rules="emailRules" prepend-icon="person" name="email" label="Email" type="email"></v-text-field>
-                  <v-text-field v-model="password" :rules="passwordRules" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                <v-toolbar dark color="primary">
+                    <v-toolbar-title>Sign in</v-toolbar-title>
+                </v-toolbar>
+                <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleSignIn">
+                    <v-card-text>
+                        <v-text-field v-model="email" :rules="emailRules" prepend-icon="person" name="email" label="Email" type="email"></v-text-field>
+                        <v-text-field v-model="password" :rules="passwordRules" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <h4 class="mt-3 mb-3">Don't have an account?
+                            <router-link to="/sign_up">Sign up</router-link>
+                        </h4>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn :loading="loading" :disabled="!isFormValid || loading" color="primary" type="submit">
+                            Sign in
+                            <span slot="loader">Loading...</span>
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
                 </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <h4 class="mt-3 mb-3">Don't have an account?
-                    <router-link to="/sign_up">Sign up</router-link>
-                </h4>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn :loading="loading" :disabled="!isFormValid || loading" @click="handleSignIn" color="primary" type="submit">
-                    Sign in
-                    <span slot="loader">Loading...</span>
-                </v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
             </v-card>
         </v-flex>
     </v-layout>
@@ -43,12 +43,12 @@ export default {
             password: '',
             emailRules: [
                 // check if email in input
-                email => !!email || 'Email is required',
-                email => /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email) || 'Email must be valid'
+                email => !!email || 'email is required',
+                email => /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email) || 'email must be valid'
             ],
             passwordRules: [
                 // check if password in input
-                password => !!password || 'Password is required'
+                password => !!password || 'password is required'
             ]
         }
     },
--- client/src/views/auth/SignUp.vue
+++ client/src/views/auth/SignUp.vue
@@ -2,31 +2,31 @@
     <v-layout align-center justify-center>
         <v-flex xs12 sm8 md6>
             <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Sign up</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-form v-model="isFormValid" lazy-validation ref="form">
-                  <v-text-field v-model="username" :rules="usernameRules" prepend-icon="person" name="username" label="Username" type="text"></v-text-field>
-                  <v-text-field v-model="email" :rules="emailRules" prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
-                  <v-text-field v-model="password" :rules="passwordRules" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                <v-toolbar dark color="primary">
+                    <v-toolbar-title>Sign up</v-toolbar-title>
+                </v-toolbar>
+                <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleSignUp">
+                    <v-card-text>
+                        <v-text-field v-model="username" :rules="usernameRules" prepend-icon="person" name="username" label="Username" type="text"></v-text-field>
+                        <v-text-field v-model="email" :rules="emailRules" prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
+                        <v-text-field v-model="password" :rules="passwordRules" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <h4 class="mt-3 mb-3">Already have an account?
+                            <router-link to="/sign_in">Sign in</router-link>
+                        </h4>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn :loading="loading" :disabled="!isFormValid || loading" @click="handleSignUp" color="primary" type="submit">
+                            Sign up
+                            <span slot="loader">Loading...</span>
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
                 </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <h4 class="mt-3 mb-3">Already have an account?
-                    <router-link to="/sign_in">Sign in</router-link>
-                </h4>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn :loading="loading" :disabled="!isFormValid || loading" @click="handleSignUp" color="primary" type="submit">
-                    Sign up
-                    <span slot="loader">Loading...</span>
-                </v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
             </v-card>
         </v-flex>
     </v-layout>
@@ -45,18 +45,18 @@ export default {
             password: '',
             usernameRules: [
                 // check if username in input
-                username => !!username || 'Username is required',
-                username => username.length >= 5 || 'Username must be at least 5 characters'
+                username => !!username || 'username is required',
+                username => username.length >= 5 || 'username must be at least 5 characters'
             ],
             emailRules: [
                 // check if email in input
-                email => !!email || 'Email is required',
-                email => /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email) || 'Email must be valid'
+                email => !!email || 'email is required',
+                email => /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email) || 'email must be valid'
             ],
             passwordRules: [
                 // check if password in input
-                password => !!password || 'Password is required',
-                password => password.length >= 8 || 'Password must be at least 8 characters'
+                password => !!password || 'password is required',
+                password => password.length >= 8 || 'password must be at least 8 characters'
             ]
         }
     },
