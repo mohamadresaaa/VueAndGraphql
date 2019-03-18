ca7caf10265ba43f336098cbd0db55a2cb8ed347
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-03-18 12:30:40 +0330

    generate random color

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,34 +1,47 @@
 <template>
-  <v-container text-xs-center>
-    <Loading v-if="loading" :loading="loading" />
-    <v-flex xs12>
-      <ul v-if="!loading">
-        <li v-for="(category, index) in categories" :key="index">
-          {{ category.title }} - {{ category.url }}
-        </li>
-      </ul>
-    </v-flex>
-  </v-container>
+  <div>
+    <v-layout v-if="!loading" justify-center align-center wrap>
+      <v-flex xs12>
+        <!-- categories -->
+        <v-layout row wrap>
+          <v-flex class="pa-2" xs6 sm4 md2 v-for="(category, index) in categories" :key="index">
+            <v-container grid-list-xl pa-0>
+              <v-card :style="`background-color: ${genrateColor()}`">
+                <v-card-text class="white--text text-xs-center">
+                  {{ category.title }}
+                </v-card-text>
+              </v-card>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import Loading from '../components/Loading';
+  import { mapGetters } from "vuex";
+  import Loading from '../components/Loading';
 
-export default {
-  name: 'home',
-  created() {
-    this.handleGetCategories();
-  },
-  computed: {
-    ...mapGetters(["loading", "categories"])
-  },
-  methods: {
-    handleGetCategories() {
-      // reach out to Vuex store, fire action that gets posts for carousel
-      this.$store.dispatch("getCategories");
+  export default {
+    name: 'home',
+    created() {
+      this.handleGetCategories();
+    },
+    computed: {
+      ...mapGetters(["loading", "categories"])
+    },
+    methods: {
+      handleGetCategories() {
+        // reach out to Vuex store, fire action that gets posts for carousel
+        this.$store.dispatch("getCategories");
+      },
+      genrateColor() {
+        return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+      }
+    },
+    components: {
+      Loading
     }
-  },
-  components: { Loading }
-};
+  };
 </script>
