c59966f8a09586e6537a914ff57b69c8fb2d392e
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-03-29 18:35:02 +0430

    update generate code process

--- server/src/lib/generator.js
+++ server/src/lib/generator.js
@@ -3,14 +3,27 @@ import errorHandle from './errorHandle';
 
 export const generateTwoFactorCode = async (user) => {
     try {
-        // create two factor code
-        let newCode = await config.database.mongodb.models.TwoFactorCode({
-            user, 
-            code: shortid.generate()
-        }).save();
+        // find code with userId
+        let twoFactorCode = await config.database.mongodb.models.TwoFactorCode.findOne({ user });
+
+        // if not, handle it
+        if(!twoFactorCode){
+            // create two factor code
+            let newCode = await config.database.mongodb.models.TwoFactorCode({
+                user, 
+                code: shortid.generate()
+            }).save();
+        
+            // return it
+            return newCode.code;
+        }
+
+        // update code
+        twoFactorCode.$set({ code: shortid.generate() });
+        await twoFactorCode.save();
         
         // return it
-        return newCode.code;
+        return twoFactorCode.code;
     } catch (err) {
         errorHandle(err);
     }
