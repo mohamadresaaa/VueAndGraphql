ebc9637b00d08fa4d1c56de606e5576f43788b72
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-04-06 18:05:55 +0430

    edit mutations

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,11 +1,11 @@
 export default {
-    setUser: (state, payload) => state.user = payload,
-    clearUser: state => state.user = null,
-    setCategories: (state, payload) => state.categories = payload,
-    setTodos: (state, payload) => state.todos = payload,
-    setNotes: (state, payload) => state.notes = payload,
-    setLoading: (state, payload) => state.loading = payload,
-    setAuthError: (state, payload) => state.authError = payload,
-    setMessage: (state, payload) => state.message = payload,
-    clearMessage: state => state.message = null
+    setUser: (state, payload) => (state.user = payload),
+    clearUser: state => (state.user = null),
+    setCategories: (state, payload) => (state.categories = payload),
+    setTodos: (state, payload) => (state.todos = payload),
+    setNotes: (state, payload) => (state.notes = payload),
+    setLoading: (state, payload) => (state.loading = payload),
+    setAuthError: (state, payload) => (state.authError = payload),
+    setMessage: (state, payload) => (state.message = payload),
+    clearMessage: state => (state.message = null)
 };
